6a1a19df40694378d217fde890073f49e4708699
vidya-piske <vidyapiske123@gmail.com>
2024-05-30 11:14:50 +0530

    In FileUploader made the setstate empty after uploaded

--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -1,20 +1,28 @@
 import { ref, uploadBytes } from '@firebase/storage';
 import { storage } from '../firebase/firebase';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 const FileUploader = () => {
   const [file, setFile] = useState(null);
+  const fileInputRef = useRef(null);
 
   const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0]; /*{first file selected by a user}*/
+    const selectedFile = e.target.files[0];
     setFile(selectedFile);
   };
 
   const handleUpload = async () => {
+    if (!file) {
+      console.log("No file selected");
+      return;
+    }
+
     try {
       console.log("Upload button clicked");
       const storageRef = ref(storage, 'images/' + file.name);
       await uploadBytes(storageRef, file);
+      setFile(null);  // Clear the state
+      fileInputRef.current.value = '';  // Clear the file input
       console.log("File Uploaded Successfully");
     } catch (e) {
       console.log("Error Uploading File: ", e);
@@ -23,7 +31,11 @@ const FileUploader = () => {
 
   return (
     <div>
-      <input type='file' onChange={handleFileChange} />
+      <input 
+        type='file' 
+        onChange={handleFileChange} 
+        ref={fileInputRef} 
+      />
       <button onClick={handleUpload}>Upload</button>
     </div>
   );
